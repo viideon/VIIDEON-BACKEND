ddd0e2d9b940911ad314e8ebb8ef69cbbea0633f
Abdul Basit <basitsattar850@gmail.com>
2020-06-17 20:20:49 +0500

    modified video schema to support campaign

--- models/videos.js
+++ models/videos.js
@@ -6,7 +6,21 @@ const videoSchema = new mongoose.Schema({
   title: { type: String, required: false },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   date: { type: Date, default: Date.now },
-  recieverEmail: { type: String, required: false }
+  recieverEmail: { type: String, required: false },
+  campaign: { type: Boolean, required: false },
+  logoProps: {
+    url: { type: String },
+    width: { type: String },
+    height: { type: String },
+    position: { type: String }
+  },
+  textProps: {
+    text: { type: String },
+    align: { type: String },
+    vAlign: { type: String },
+    textColor: { type: String },
+    fontSize: { type: Number }
+  }
 });
 videoSchema.index({ title: "text" });
 module.exports = mongoose.model("Video", videoSchema);
