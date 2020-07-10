d15ccb680e6dd0b58ebb85eb3ff0d3d4b5875f95
Abdul Basit <basitsattar850@gmail.com>
2020-07-11 01:15:57 +0500

    added campaign count

--- services/videoService.js
+++ services/videoService.js
@@ -4,7 +4,7 @@ const updateVideo = (id, video) => {
   return Video.findByIdAndUpdate(id, video, { new: true });
 };
 
-const deleteVideo = (videoId) => {
+const deleteVideo = videoId => {
   return Video.deleteOne({ _id: videoId });
 };
 
@@ -19,8 +19,8 @@ const findUserVideoByTitle = (userId, page, search) => {
   return Video.find({
     $and: [
       { userId: userId },
-      { title: { $regex: new RegExp(".*" + search + ".*"), $options: "i" } },
-    ],
+      { title: { $regex: new RegExp(".*" + search + ".*"), $options: "i" } }
+    ]
   }).sort({ date: -1 });
   // .skip((page - 1) * 9)
   // .limit(9);
@@ -37,8 +37,8 @@ const findUserCamaignVideoByTitle = (userId, page, search) => {
   return Video.find({
     $and: [
       { userId: userId, campaign: true },
-      { title: { $regex: new RegExp(".*" + search + ".*"), $options: "i" } },
-    ],
+      { title: { $regex: new RegExp(".*" + search + ".*"), $options: "i" } }
+    ]
   }).sort({ date: -1 });
   // .skip((page - 1) * 9)
   // .limit(9);
@@ -47,28 +47,38 @@ const findUserCamaignVideoByTitle = (userId, page, search) => {
 const getAllVideos = () => {
   return Video.find();
 };
-const findVideoByUrl = (url) => {
+const findVideoByUrl = url => {
   return Video.find({ url: url });
 };
-const findVideoById = (id) => {
+const findVideoById = id => {
   return Video.findOne({ _id: id });
 };
-const getCampaignVideos = (id) => {
+const getCampaignVideos = id => {
   return Video.find({ userId: id, campaign: true });
 };
-const getVideoCount = (id) => {
-  const count = Video.countDocuments({ userId: id }, function (err, count) {
+const getVideoCount = id => {
+  const count = Video.countDocuments({ userId: id }, function(err, count) {
     return count;
   });
   return count;
 };
+const getCampaignCount = id => {
+  const count = Video.countDocuments({ userId: id, campaign: true }, function(
+    err,
+    count
+  ) {
+    return count;
+  });
+  return count;
+};
+
 const incrementVideoEmail = (_id, count) => {
   return Video.updateOne({ _id }, { $inc: { emailShareCount: count } });
 };
-const incrementVideoViews = (_id) => {
+const incrementVideoViews = _id => {
   return Video.updateOne({ _id }, { $inc: { views: 1 } });
 };
-const incrementVideoWatch = (_id) => {
+const incrementVideoWatch = _id => {
   return Video.updateOne({ _id }, { $inc: { watch: 1 } });
 };
 module.exports = {
@@ -86,4 +96,5 @@ module.exports = {
   getCampaignVideos,
   findUserCamaignVideoByTitle,
   findUserCampaignVideo,
+  getCampaignCount
 };
