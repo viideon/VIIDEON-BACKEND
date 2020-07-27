8ccd2de7943a36a311c73c7d58d9f26a2846596a
Abdul Basit <basitsattar850@gmail.com>
2020-07-27 17:43:15 +0500

    modified app domain in email heplers

--- helpers/email.js
+++ helpers/email.js
@@ -16,7 +16,7 @@ const sendEmail = async (user, req, res) => {
     from: `vidionPro<${process.env.FROM_EMAIL}>`,
     subject: "Account Verification",
     text: `Hi ${user.firstName} ${user.lastName} \n 
-                Please click on this link to verify your email  https://vidionpro.000webhostapp.com/login/VerifyEmail?code=${token.token} . \n\n`
+                Please click on this link to verify your email  ${process.env.APP_DOMAIN}/login/VerifyEmail?code=${token.token} . \n\n`
   };
   try {
     const tokenSaved = await token.save();
@@ -39,7 +39,7 @@ const sendForGotEmail = async (user, token) => {
     subject: "Reset password link",
     text: "Some useless text",
     html: `<p>You are receiving this because you (or someone else) have requested to reset  the password for your account.\n\n Please click on the following link, or paste this into your browser to complete the process:\n\n
-        <a href="https://vidionpro.000webhostapp.com/resetpassword?code=${token.token}">https://vidionpro.000webhostapp.com/resetpassword?code=${token.token}</a> \n\n If you did not request this, please ignore this email and your password will remain unchanged.\n </p>`
+        <a href="${process.env.APP_DOMAIN}/resetpassword?code=${token.token}">${process.env.APP_DOMAIN}/resetpassword?code=${token.token}</a> \n\n If you did not request this, please ignore this email and your password will remain unchanged.\n </p>`
   };
   try {
     const tokenSaved = await token.save();
