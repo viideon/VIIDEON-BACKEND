4ac7604a54813c12b038d783073322d10cac3be2
Abdul Basit <basitsattar850@gmail.com>
2020-08-20 15:23:32 +0500

    add music prop to video model

--- models/videos.js
+++ models/videos.js
@@ -27,6 +27,9 @@ const videoSchema = new mongoose.Schema({
     textColor: { type: String },
     fontSize: { type: Number }
   },
+  musicProps:{
+    url:{type:String}
+  }
   description: { type: String }
 });
 videoSchema.index({ title: "text" });
