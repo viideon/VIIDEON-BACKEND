500245eece48d39e667b9627c701b8c666f52d06
Michael Blanton <michael@blanton.cloud>
2022-08-20 22:03:16 -0700

    #4 - updated emailConfig model

--- models/emailConfig.js
+++ models/emailConfig.js
@@ -1,9 +1,20 @@
-const mongoose = require("mongoose");
+const dynamoose = require("dynamoose");
 
 const userModel = require('./user');
 
-const emailConfigSchema = new mongoose.Schema({
-  userId: { type: userModel.model },
+const schema = new dynamoose.Schema({
+  _id: {
+    type: String,
+    default: uuid(),
+    hashKey: true,
+  },
+  userId: {
+    type: userModel.model,
+    index: {
+      name: 'gidx-userId',
+      global: true,
+    },
+  },
   date: { type: Date, default: Date.now },
   userEmail: { type: String },
   tokenObj: {
@@ -16,4 +27,36 @@ const emailConfigSchema = new mongoose.Schema({
   }
 });
 
-module.exports = mongoose.model("emailConfig", emailConfigSchema);
+module.exports.model = dynamoose.model(process.env.EMAIL_CONFIG_TABLE_NAME, schema);
+
+module.exports.create = data => {
+  return this.model.create(data);
+}
+
+module.exports.getByUserId = userId => {
+  return new Promise((resolve, reject) => {
+    this.model.query('userId').eq(userId).using('gidx-userId').all().exec((err, response) => {
+      if (err) {
+        return reject(err);
+      }
+
+      if (response.length > 1) {
+        return reject('Multiple users found');
+      }
+
+      if (response.length === 0) {
+        return resolve(null);
+      }
+
+      resolve(response[0]);
+    });
+  });
+}
+
+module.exports.get = id => {
+  return this.model.get(id);
+}
+
+module.exports.delete = id => {
+  return this.model.delete(id);
+}
